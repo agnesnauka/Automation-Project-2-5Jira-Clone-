5642c738648a61c72d8ece8eff065fbac8ad2823
agnesnauka <148770326+agnesnauka@users.noreply.github.com>
2023-11-28 11:33:13 +0200

    Assignment 1 Sprint 2

--- cypress/e2e/issue-comments.cy.js
+++ cypress/e2e/issue-comments.cy.js
@@ -68,4 +68,59 @@ describe('Issue comments creating, editing and deleting', () => {
             .find('[data-testid="issue-comment"]')
             .should('not.exist');
     });
-});
+
+    //this is the new part
+    it('Should create, edit and delete a comment successfully', () => {
+        const comment = 'Hello';
+        const editComment = 'Hello there!';
+
+        getIssueDetailsModal().within(() => {
+            cy.contains('Add a comment...')
+                .click();
+
+            cy.get('textarea[placeholder="Add a comment..."]').type(comment);
+
+            cy.contains('button', 'Save')
+                .click()
+                .should('not.exist');
+
+            cy.contains('Add a comment...').should('exist');
+            cy.get('[data-testid="issue-comment"]').should('contain', comment);
+
+            cy.get('[data-testid="issue-comment"]')
+                .first()
+                .contains('Edit')
+                .click()
+                .should('not.exist');
+
+            cy.get('textarea[placeholder="Add a comment..."]')
+                .should('contain', comment)
+                .clear()
+                .type(editComment);
+
+            cy.contains('button', 'Save')
+                .click()
+                .should('not.exist');
+
+            cy.get('[data-testid="issue-comment"]')
+                .should('contain', 'Edit')
+                .and('contain', editComment);
+
+            cy.get('[data-testid="issue-comment"]')
+                .first()
+                .contains('Delete')
+                .click();
+        });
+    
+        cy.get('[data-testid="modal:confirm"]')
+            .contains('button', 'Delete comment')
+            .click()
+            .should('not.exist');
+    
+        getIssueDetailsModal()
+            .find('[data-testid="issue-comment"]')
+            .contains(editComment)
+            .should('not.exist');
+
+    });
+});  
